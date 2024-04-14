b469c2ae6e78f3c5179a5be41fe8b0cfda17aa1f
rytst <r.saito0111@gmail.com>
2024-04-14 20:21:34 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# Reinforcement learning (DQN)
+
+# train a `Deep Q Learning` agent on the `CartPole-v1` task
+
+
+# The agent has to decide between two actions - moving the cart left or right - so that the pole attached to it stays upright.
+
+
+# Packages
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+env = gym.make("CartPole-v1")
+
+
+# Replay Memory
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
